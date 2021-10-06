6a471f28b1a581d21686eb6dff88841e7d133570
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-06 01:44:48 -0700

    typeDefs rough draft

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,30 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [bookSchema]
+    }
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        users: [User]!
+        user(userId: ID!): User
+        books: [Book]
+        me: Profile
+    }
+`;

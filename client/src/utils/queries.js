03b3de7295f65df7348b01db7a8c4642ffa1619a
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-07 20:43:06 -0700

    wrote queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,11 @@
 import { gql } from '@apollo/client';
 
-export const 
+export const GET_ME = gql`
+    query me {
+        _id
+        username
+        email
+        password
+        savedBooks
+    }
+`;

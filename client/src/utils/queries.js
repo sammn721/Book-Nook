5b8882c835b9310fa0c379cd5ec09f004ae242ed
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-10 16:34:43 -0700

    fixed query me

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,11 +2,18 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
     query me {
-        _id
-        username
-        email
-        password
-        savedBooks
-        bookCount
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
     }
 `;

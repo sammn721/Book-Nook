0b2e538b35e7fafea98ce8a59bf4905cd8533fbd
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-10 16:29:37 -0700

    had something defined twice in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,8 +19,7 @@ const typeDefs = gql`
     type Book {
         bookId: String
         authors: [String]
-        description: String!
-        bookId: String!
+        description: String
         title: String
         image: String
         link: String

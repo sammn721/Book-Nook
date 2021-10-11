4645e799ff6b2234bba9a3aa254aa08b82c9c9bf
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-10 17:24:05 -0700

    LOGIN WORKS AGAIN. Not saving books tho.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -28,15 +28,10 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      })
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);

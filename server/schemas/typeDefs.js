fd4615bb5543619a27c49b8ae9ba5d381e55efdb
sammn721 <82857538+sammn721@users.noreply.github.com>
2021-10-22 15:36:34 -0700

    removed input type bookInput

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    input BookInput {
-        authors: [String]
-        description: String
-        bookId: String!
-        image: String
-        link: String
-        title: String
-    }
     type User {
         _id: ID!
         username: String
